3b0d92838bf2a9e268303e3d7fcac83303e65577
Nascimento95 <marco-pereira@hotmail.fr>
2021-12-08 11:54:38 +0100

    exo fini manque juste a mettre les drapeaux qui corresponde a la langue

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require("express")
 const app = express()
 const port = 5000
+// je stock dans une variable les information de mon json
+const translate = require('./translate.json')
+// les 3 ligne pour faire fonctionner handlebars
+const engine = require("express-handlebars").engine
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+
+app.get('/langage', (req, res) => {
+    res.json(translate);
+});
+
+
+app.get("/:lang?", (req,res) => {
+    // const {lang} = req.query
+    let {lang} = req.params
+    if(!lang || lang !== "fr" && lang !== "en" && lang !== "es" ){
+        lang = "fr"
+    }
+    console.log("valeur =>",lang);
+    res.render('home',{
+        
+        fr:translate[lang].pageTitle,
+        title:translate[lang].title
+    } )
+
+})
+
 
 app.listen(port, () =>{
     console.log(`serveur en marche sur le port:${port}`);
